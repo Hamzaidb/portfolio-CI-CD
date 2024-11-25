bbdc6dcc56eb5c9584233daa306fc4e0ed1f105e
Hamza_Idb <hamza.idblhy@gmail.com>
2024-11-25 11:21:19 +0100

    tst

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export',
+  basePath: '/portfolio-CI-CD',
+  assetPrefix: '/portfolio-CI-CD',
 };
+
+ 
 
 export default nextConfig;
